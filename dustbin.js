d4142ea4bef0fcbe2980259e558dc5fba209773f
OmAgnihotri <72681406+OmAgnihotri@users.noreply.github.com>
2020-11-20 20:22:07 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,26 @@
+class Dustbin {
+    //constructor is automatically called when a object is created useing this class. this key word reffers to the object that is being created
+    constructor(x,y,w,h){
+    var options={
+        isStatic:true
+      }    
+    this.body =Bodies.rectangle(x,y,w,h,options);
+    World.add(world,this.body);
+    this.width=w;
+    this.height=h;
+    this.image = loadImage("sprites/dustbingreen.png");
+    }  
+    display(){
+    var pos=this.body.position;   
+    var angle=this.body.angle;
+    push ();
+    translate(pos.x,pos.y)
+    rotate (angle);
+    fill("white");
+    rectMode(CENTER);  
+    rect(0,0,this.width,this.height);  
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop ();
+    }  
+    }
---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+paperIMG=loadImage("paper.png");
+dustbinIMG=loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+
+	base = new Ground(400,550,800,10);
+	 
+	garbage = new Paper(180,500,20); 
+   
+	dustbin1 = new Dustbin(560,480,20,100);
+	dustbin2 = new Dustbin(650,540,200,20);
+	dustbin3 = new Dustbin(740,480,20,100);
+	dustbinIMG
+	
+
+	Engine.run(engine);
+	
+}
+
+function draw() {
+	rectMode(CENTER);
+	background(0);
+	base.display();
+
+	garbage.display();
+
+	dustbin1.display();
+	dustbin2.display();
+	dustbin3.display();
+
+
+  
+  
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(garbage.body,garbage.body.position,{x:55,y:-55});
+	}
+   }
+
+
+
